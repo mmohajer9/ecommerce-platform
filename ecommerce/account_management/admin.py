3bdde71c853369f771f9035192aea16b278aeee8
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-14 23:34:38 +0430

    models created for account_management and admin panel configured

--- ecommerce/account_management/admin.py
+++ ecommerce/account_management/admin.py
@@ -7,6 +7,10 @@ from django.contrib.auth import get_user_model
 
 from .models import (
     User,
+    Country,
+    State,
+    City,
+    Address,
 )
 
 class MyUserAdmin(UserAdmin):
@@ -25,3 +29,48 @@ MyUserAdmin.fieldsets += (
 
 admin.site.register(User, MyUserAdmin)
 admin.site.site_header = _("E-Commerce Platform")
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state_count', 'city_count')
+    # list_filter = (['status'])
+    # search_fields = ('title', 'slug')
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'city_count')
+    # list_filter = (['status'])
+    # search_fields = ('title', 'slug')
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(State, StateAdmin)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'address_count')
+    # list_filter = (['status'])
+    # search_fields = ('title', 'slug')
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(City, CityAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'full_name', 
+        'id', 'country', 'state',
+        'city', 'postal_code', 
+        'address_line1', 'address_line2'
+    )
+    # list_filter = (['status'])
+    search_fields = ('user__username', 'postal_code__startswith')
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Address, AddressAdmin)
--- ecommerce/account_management/models.py
+++ ecommerce/account_management/models.py
@@ -11,6 +11,67 @@ from django.utils.html import format_html
 from django.utils import timezone
 from datetime import timedelta
 
+
+class Country(models.Model):
+   
+    name = models.CharField(max_length=100 , unique = True , verbose_name=_("Country Name"))
+
+    def __str__(self):
+        return self.name
+
+    def state_count(self):
+        return (self.state_set.count())
+    state_count.short_description = _("States Count")
+
+    def city_count(self):
+        return (City.objects.filter(state__country = self).count())
+    city_count.short_description = _("Cities Count")
+
+    class Meta:
+        verbose_name = _('Country')
+        verbose_name_plural = _('Countries')
+
+
+class State(models.Model):
+    
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name=_("Country"))   
+    name = models.CharField(max_length=100, verbose_name=_("State Name"))
+
+    def city_count(self):
+        return (self.city_set.count())
+    city_count.short_description = _("Cities Count")
+
+
+    class Meta:
+        verbose_name = _('State')
+        verbose_name_plural = _('States')
+        unique_together = ('country','name')
+
+        
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    
+    state = models.ForeignKey(State , on_delete=models.CASCADE , verbose_name=_("State"))    
+    name = models.CharField(max_length=100 , blank=True, null=True , verbose_name=_("City Name"))
+
+    class Meta:
+        verbose_name = _('City')
+        verbose_name_plural = _('Cities')
+        unique_together = ('state','name')
+
+
+    def address_count(self):
+        return (self.address_set.count())
+    address_count.short_description = _('Addresses Count')
+
+    def __str__(self):
+        return self.name
+
+
+
 def profile_pic_upload_to(instance, filename):
     return f'account_management/{instance.username}/profile_pics/{filename}'
 # Create your models here.
@@ -46,3 +107,26 @@ class User(AbstractUser):
         verbose_name_plural = _('Users')
         # unique_together = ('email',)
 
+class Address(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("User"))
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name=_("City"))
+    postal_code = models.CharField(max_length=20, verbose_name=_("Postal/Zip Code"))
+    address_line1 = models.CharField(max_length=100, verbose_name=_("Address Line 1"))
+    address_line2 = models.CharField(max_length=100 , blank=True, null=True, verbose_name=_("Address Line 2"))
+
+
+    def __str__(self):
+        return self.user.full_name()
+
+    def country(self):
+        return self.city.state.country
+    country.short_description = _('Country') 
+
+    def state(self):
+        return self.city.state
+    state.short_description = _('State')
+
+    def full_name(self):
+        return self.user.get_full_name()
+    full_name.short_description = _('Full Name')
