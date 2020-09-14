43e8359169fc56f4f202d3a84cc2be1e3a2c2647
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-14 15:07:19 +0430

    project initialized with settings configured

---
+++ ecommerce/config/urls.py
@@ -0,0 +1,61 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework import permissions  # new
+from drf_yasg.views import get_schema_view  # new
+from drf_yasg import openapi  # new
+
+schema_view = get_schema_view(  # new
+    openapi.Info(
+        title="E-Commerce Platform",
+        default_version='v1',
+        description="Police Market Like Ebay and Etsy",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mmohajer98@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+
+    #? admin panel and built-in urls
+    path('admin/', admin.site.urls),
+    
+    #? documentation generator urls
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    #? authentication and authorization
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+
+    #? applications urls
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#? handler404 = a view for handling 404 not found page -> def view(request , Exception = None):
+#? handler500 = same as 404 ... 
