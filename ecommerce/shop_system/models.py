dfc81144483753dd80fe38129b4dd475ef86f51d
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-15 16:16:22 +0430

    category added to shop_system

--- ecommerce/shop_system/models.py
+++ ecommerce/shop_system/models.py
@@ -16,13 +16,37 @@ from djmoney.money import Money #* for using for searchs and filters
 
 # Create your models here.
 
+class Category(models.Model):
+	parent = models.ForeignKey(
+        'self', default=None, 
+        null=True, blank=True, 
+        on_delete=models.SET_NULL, 
+        related_name='subcategories', 
+        verbose_name=_("SubCategory")
+    )
+	title = models.CharField(max_length=200, verbose_name=_("Title"))
+	slug = models.SlugField(max_length=100, unique=True, verbose_name=_("Slug"))
+	class Meta:
+		verbose_name = _("Category")
+		verbose_name_plural = _("Categories")
+		ordering = ['parent__id',]
+
+	def __str__(self):
+		return self.title
+
+
 USER_MODEL = get_user_model()
 
 class Product(models.Model):
 
     user = models.ForeignKey(USER_MODEL, related_name='products', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200, verbose_name="Title")
+    title = models.CharField(max_length=200, verbose_name=_("Title"))
     slug = AutoSlugField(populate_from='get_title_slug',editable=True,always_update=True)
+    category = models.ForeignKey(
+        Category, related_name='products', 
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
     created_date = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name=_("Created Date"))
     updated_date = models.DateTimeField(auto_now=True, verbose_name=_("Updated Date"))
 
