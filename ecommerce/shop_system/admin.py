bf2a22a61780474c64ca7b1aa6e09b4aca49fbbd
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-16 12:02:46 +0430

    order item added with model admin

--- ecommerce/shop_system/admin.py
+++ ecommerce/shop_system/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import (
     Product,
-    Category
+    Category,
+    Order,
+    OrderItem
 )
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'title','user',
+        'title','id','user',
         'image_thumbnail',
         'slug','categories_list',
         'price','discount_price',
@@ -22,7 +24,7 @@ admin.site.register(Product , ProductAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
-        'title','parent',
+        'title','id','parent',
         'slug','subcategories_list',
         'created_date','updated_date',
     )
@@ -32,3 +34,28 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Category , CategoryAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'user','id','created_date',
+        'updated_date','status'
+    )
+    # list_filter = ([])
+    # search_fields = ('name', 'university')
+    
+    # prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Order , OrderAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'product','id','order',
+        'quantity','date_added'
+    )
+    # list_filter = ([])
+    # search_fields = ('name', 'university')
+    
+    # prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(OrderItem , OrderItemAdmin)
--- ecommerce/shop_system/models.py
+++ ecommerce/shop_system/models.py
@@ -141,3 +141,8 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        # db_table = ''
+        # managed = True
+        verbose_name = 'Order Item'
+        verbose_name_plural = 'Order Items'
