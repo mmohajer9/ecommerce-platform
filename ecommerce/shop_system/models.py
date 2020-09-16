cda94d03d6244acc02bfd5e0271459d2bd09bd30
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-16 11:56:25 +0430

    OrderItem model added

--- ecommerce/shop_system/models.py
+++ ecommerce/shop_system/models.py
@@ -134,3 +134,10 @@ class Order(models.Model):
         # managed = True
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
