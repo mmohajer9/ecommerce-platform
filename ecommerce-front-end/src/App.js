7203d01fa8d9285d6e9338c89e1162ed89d066e2
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-24 14:43:19 +0330

    folder structure revised

--- ecommerce-front-end/src/App.js
+++ ecommerce-front-end/src/App.js
@@ -1,9 +1,18 @@
+import { ThemeProvider } from "@material-ui/core";
 import React from "react";
 
 import "./App.css";
+import { darkTheme, theme } from "./common/theme";
 
 const App = () => {
-  return <div>Hello</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : theme}>
+
+    </ThemeProvider>
+  )
 };
 
 export default App;
--- ecommerce-front-end/src/common/theme.js
+++ ecommerce-front-end/src/common/theme.js
@@ -2,6 +2,10 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({});
 
-const darkTheme = createMuiTheme({});
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 export { theme, darkTheme };
