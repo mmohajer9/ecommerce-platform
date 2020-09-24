ea90e1f1b5fcd35628fcacdb0b76c23a8c3c0fa9
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-24 16:26:30 +0330

    add loadable component for code spliting and dynamic import

--- ecommerce-front-end/src/components/App/App.js
+++ ecommerce-front-end/src/components/App/App.js
@@ -1,8 +1,18 @@
-import { ThemeProvider } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import React from "react";
+import loadable from "@loadable/component";
 
 import "./App.css";
 import { darkTheme, theme } from "../../common/theme";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  /*  Link */
+} from "react-router-dom";
+
+//? Components Are Dynamically Added
+const Homepage = loadable(() => import("../../layouts/Homepage/Homepage"));
 
 const App = () => {
   // eslint-disable-next-line no-unused-vars
@@ -10,9 +20,16 @@ const App = () => {
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : theme}>
-
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
-  )
+  );
 };
 
 export default App;
---
+++ ecommerce-front-end/src/layouts/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+
+
+    return (
+        <div>
+            this is homepage
+        </div>
+    )
+};
+
+export default Homepage;
