26d756c784a1e9fd29e4f90e7289c7090c747a6c
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-09-26 01:03:36 +0330

    some templates added

--- ecommerce-front-end/src/components/Footer/Footer.jsx
+++ ecommerce-front-end/src/components/Footer/Footer.jsx
@@ -1,27 +1,53 @@
-import { Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: theme.palette.HeaderBgColor,
-    padding : "5em 0"
+    display: "flex",
+    flexDirection: "column",
+  },
+  main: {
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(8, 2),
+    marginTop: "auto",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800],
   },
 }));
 
-const Footer = () => {
+export default function StickyFooter() {
   const classes = useStyles();
-  return (
-    <Grid
-      container
-      className={classes.root}
-      direction="column"
-      alignItems="center"
-      justify="center"
-    >
-      <Grid item>Copyright 2020</Grid>
-    </Grid>
-  );
-};
 
-export default Footer;
+  return (
+    <div className={classes.root}>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
